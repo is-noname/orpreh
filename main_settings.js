ae9d3a3a07014e45dfff54f3342c7a1ec0978c62
gudboi <147923821+is-noname@users.noreply.github.com>
2025-06-23 18:51:37 +0200

    Update main_settings.js

--- main_settings.js
+++ main_settings.js
@@ -3,7 +3,7 @@
 
 const tools = [
     { name: 'Herbert CEE Helper', file: 'tools/Herbert_CEE_Helper_Test.html', icon: '🌱', category: 'Testecke' },
-    { name: 'SM-Vergleich', file: './tools/sm_vergleich.html', icon: '🌱', category: 'Testecke' },
+    { name: 'SM-Vergleich', file: 'tools/sm_vergleich.html', icon: '🔧', category: 'Testecke' },
     
     // === NEUE TOOLS HIER HINZUFÜGEN ===
     // { name: 'Mein neues Tool', file: 'tools/mein_tool.html', icon: '🔧', category: 'Utilities' },
